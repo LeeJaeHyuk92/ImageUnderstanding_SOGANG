391959938dd621bc19528301db6010eb583c19aa
LeeJaeHyuk92 <leejh920917@gmail.com>
2017-09-27 22:02:37 +0900

    Graph_isomorphism & networkx

---
+++ GraphIsomorphism/graph_isomorphsim.py
@@ -0,0 +1,43 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+from networkx.algorithms import isomorphism
+
+
+def graph_matching(graph_1, graph_2):
+    GM = isomorphism.GraphMatcher(graph_1, graph_2)
+    GM.is_isomorphic()
+    pi = GM.mapping
+    print(GM.mapping)
+
+    plt.figure()
+    plt.title('network_G1')
+    nx.draw_networkx(G1)
+
+    plt.figure()
+    plt.title('network_G2')
+    nx.draw_networkx(G2)
+
+    return pi
+
+
+if __name__ == "__main__":
+    G1 = nx.path_graph(4)
+    G2 = nx.path_graph(4)
+    result = graph_matching(G1, G2)
+
+    G1 = nx.Graph()
+    G1.add_edges_from([('a', 'g'), ('a', 'h'), ('a', 'i'),
+                       ('b', 'g'), ('b', 'h'), ('b', 'j'),
+                       ('c', 'g'), ('c', 'i'), ('c', 'j'),
+                       ('d', 'h'), ('d', 'i'), ('d', 'j')])
+    G2 = nx.Graph()
+    G2.add_edges_from([(1, 2), (1, 4), (1, 5),
+                      (2, 3), (2, 6),
+                      (3, 4), (3, 7),
+                      (4, 8),
+                      (5, 6), (5, 8),
+                      (6, 7),
+                      (7, 8)])
+    result = graph_matching(G1, G2)
+
+    plt.show()
