c112cb16154ceddb33b6e5f2490408644bbab172
LeeJaeHyuk92 <leejh920917@gmail.com>
2017-09-27 22:01:55 +0900

    homework 1

--- Homework1/PCA.py
+++ Homework1/PCA.py
@@ -3,6 +3,13 @@ import numpy as np
 import os
 
 
+def scaling(arr):
+    dif_min = 0 - arr.min()
+    arr = arr + dif_min
+    arr = arr * 255 / arr.max()
+    return arr
+
+
 def cov(x):
     """
     compute eigenvalue & eigenvector
@@ -19,36 +26,55 @@ def cov(x):
     return cov_x
 
 
-def save_images(y, shape):
+def save_images(y, shape, name='default'):
     y = np.reshape(y, shape)
+    #y = y.astype(np.uint8)
     for idx in range(shape[0]):
-        cv2.imwrite('PCimage'+str(idx)+'.png', y[idx, :, :])
-        cv2.imwrite('reverse_PCimage' + str(idx) + '.png', np.absolute(y[idx, :, :]))
+        cv2.imwrite(str(name)+'_image'+str(idx)+'.png', y[idx, :, :])
 
 
 def main():
-    x = np.array([cv2.imread(idx, cv2.IMREAD_GRAYSCALE) for idx in ['images/Fig1138_a.tif',
-                                                                   'images/Fig1138_b.tif',
-                                                                   'images/Fig1138_c.tif',
-                                                                   'images/Fig1138_d.tif',
-                                                                   'images/Fig1138_e.tif',
-                                                                   'images/Fig1138_f.tif']],
+    img = np.array([cv2.imread(idx,cv2.IMREAD_GRAYSCALE)
+                    for idx in ['images/Fig1138_a.tif',
+                                'images/Fig1138_b.tif',
+                                'images/Fig1138_c.tif',
+                                'images/Fig1138_d.tif',
+                                'images/Fig1138_e.tif',
+                                'images/Fig1138_f.tif']],
                  dtype=np.float)
-    shape = x.shape
-    x = np.reshape(x, (6,-1))
+    shape = img.shape
+    x = np.reshape(img, (6,-1))
     # cov_x = np.cov(x, rowvar=True)
     cov_x = cov(x)
-    e, e_vec = np.linalg.eigh(cov_x)        # ascending order
-    trans = e_vec[::-1]
+    e, e_vec = np.linalg.eig(cov_x)        # decending order
+    trans = e_vec.T
 
-    x = x.T                     # observation, variable = (pixels, images)
-    mean_x = np.mean(x, axis=0)
-    x_ = x - mean_x
+    trans[0] = -trans[0]
+    trans[2] = -trans[2]
+    trans[3] = -trans[3]
+    trans[4] = -trans[4]
 
-    y = np.dot(trans, x_)
+    x_m = x.copy()
+    mean_x = np.mean(x, axis=1)
+    for idx in range(mean_x.shape[0]):
+        x_m[idx, :] = x_m[idx, :] - mean_x[idx]
 
-    save_images(y, shape)
+    # Principle Component images
+    y = np.dot(trans, x_m)
+    y_ = scaling(y)
+    save_images(y_, shape, name='PCI')
 
+    # Reconstruction with 2 PCI
+    num = 2
+    x_ = np.dot(trans[0:num].T, y[0:num])
+    for idx in range(mean_x.shape[0]):
+        x_[idx, :] = x_[idx, :] + mean_x[idx]
+    save_images(x_, shape, name='Rec'+str(num))
+
+    # Difference between origin and reconstructed images
+    x_ = scaling(x_)
+    d_ = x - x_
+    save_images(d_, shape, name='Diff')
 
 if __name__=='__main__':
     main()
